6226a1c9b1a9525aa0cf2ed0a4993a89d062911e
Daniel Winkler <dw@dwinkler.org>
2026-05-21 21:10:30 +1000

    make lua package loading conditional on cat

--- plugin/22_languages.lua
+++ plugin/22_languages.lua
@@ -1,6 +1,7 @@
 local add = Config.add
 local now_if_args = Config.now_if_args
 local later = MiniDeps.later
+local nix = require('config.nix')
 
 if not Config.isNixCats then
   local m_add = MiniDeps.add
@@ -12,19 +13,21 @@ end
 
 -- lua
 later(function()
-  add("luvit-meta")
-  add("lazydev")
-  require("lazydev").setup({
-    library = {
-      -- See the configuration section for more details
-      -- Load luvit types when the `vim.uv` word is found
-      "lua",
-      "mini.nvim",
-      "MiniDeps",
-      { path = "luvit-meta/library", words = { "vim%.uv" } },
-      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-    },
-  })
+  if nix.get_cat("lua", false) then
+    add("luvit-meta")
+    add("lazydev")
+    require("lazydev").setup({
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        "lua",
+        "mini.nvim",
+        "MiniDeps",
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    })
+  end
 end)
 
 -- Markdown
