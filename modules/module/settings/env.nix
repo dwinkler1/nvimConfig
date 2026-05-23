740f86ad7b3494f51e1e051bf709aedcaa0ec11e
Daniel Winkler <dw@dwinkler.org>
2026-05-23 23:41:48 +1000

    hopefully R fix

--- modules/module/settings/env.nix
+++ modules/module/settings/env.nix
@@ -13,7 +13,7 @@
     })
     (lib.mkIf (config.cats.r or false) {
       RNVIM_COMPLDIR = "$PWD/.r-compl";
-      R_LIBS_USER = "${pkgs.nvimcom}/library:$PWD/.r-libs";
+      R_LIBS_USER = "$PWD/.r-libs";
       TMPDIR = "$PWD/.r-tmp";
     })
   ];
@@ -21,7 +21,7 @@
   # Environment variables with defaults (can be overridden by user)
   config.envDefault = lib.mkMerge [
     (lib.mkIf (config.cats.r or false) {
-      R_LIBS_USER = "${pkgs.nvimcom}/library:$PWD/.r-libs";
+      R_LIBS_USER = "$PWD/.r-libs";
     })
   ];
 }
